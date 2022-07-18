fe86f9d0eda69acef9b1c7bd9fb53c3baca57db6
ankitrssc786 <57948916+ankitrssc786@users.noreply.github.com>
2022-07-18 15:40:26 +0530

    Update HoodServiceImpl.java

--- test-project/src/main/java/com/test/project/controller/service/impl/HoodServiceImpl.java
+++ test-project/src/main/java/com/test/project/controller/service/impl/HoodServiceImpl.java
@@ -36,19 +36,47 @@ public class HoodServiceImpl implements HoodService {
 
 	static List<Integer> digSum(int n, List<Integer> newHoodFillers) {
 		int sum = 0;
-		while (n > 0 || sum > 2) {
+		while (n > 0 || sum > 1) {
 			if (n >= 10) {
 				n = n - 10;
 				sum = n;
 				newHoodFillers.add(10);
-			} else if (n >= 5) {
+			} else if (n >= 9) {
+				n = n - 9;
+				sum = n;
+				newHoodFillers.add(9);
+			}  else if (n >= 8) {
+				n = n - 8;
+				sum = n;
+				newHoodFillers.add(8);
+			}  else if (n >= 7) {
+				n = n - 7;
+				sum = n;
+				newHoodFillers.add(7);
+			}  else if (n >= 6) {
+				n = n - 6;
+				sum = n;
+				newHoodFillers.add(6);
+			}  else if (n >= 5) {
 				n = n - 5;
 				sum = n;
 				newHoodFillers.add(5);
+			}  else if (n >= 4) {
+				n = n - 4;
+				sum = n;
+				newHoodFillers.add(4);
+			}  else if (n >= 3) {
+				n = n - 3;
+				sum = n;
+				newHoodFillers.add(3);
 			} else if (n >= 2) {
 				n = n - 2;
 				sum = n;
 				newHoodFillers.add(2);
+			} else if (n >= 1) {
+				n = n - 1;
+				sum = n;
+				newHoodFillers.add(1);
 			}
 		}
 		return newHoodFillers;
